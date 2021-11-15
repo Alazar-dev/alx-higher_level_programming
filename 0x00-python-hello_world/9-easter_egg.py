682a41427814ad8736444bd685b5a39a000efbe7
Alazar-dev <mealazartadesse@gmail.com>
2021-11-16 00:17:54 +0300

    easter-egg

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # print("The Zen of Python, by Tim Peters \n\n Beautiful is better than ugly.\n Explicit is better than implicit.\n Simple is better than complex.\n"
 # Complex is better than complicated.\n
 # Flat is better than nested.\n
