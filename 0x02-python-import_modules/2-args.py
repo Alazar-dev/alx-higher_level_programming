63cd14c0b16f0824f0e964b756404f2b185c4305
Alazar-dev <mealazartadesse@gmail.com>
2021-11-19 04:54:40 +0300

    format code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import sys
 
+
 def printArguments():
+
     length = len(sys.argv)
     if (length == 1):
         print("0 arguments.")
@@ -14,5 +16,6 @@ def printArguments():
         for i in range(1, length):
             print("{}: {}".format(i, sys.argv[i]))
 
-if __name__ ==" __main__":
+
+if __name__ == " __main__":
     printArguments()
