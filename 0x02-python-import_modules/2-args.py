4c6c4b9cf8fb62f6d6fc5f57e33d5d87aba15c82
Alazar-dev <mealazartadesse@gmail.com>
2021-11-25 15:24:49 +0300

    updated script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,12 +5,12 @@ import sys
 def numberofarguments():
 
     length = len(sys.argv)
-
     if length == 1:
         print("0 argument")
     elif length == 2:
         print("{} argument: ".format(length - 1))
         print("{}: {}".format(1, sys.argv[1]))
+
     else:
         print("{} arguments: ".format(length - 1))
         for i in range(1, length):
