7d71a09dad05c36d999fcc48407ec6a44bd13751
Alazar-dev <mealazartadesse@gmail.com>
2021-11-25 14:06:42 +0300

    first calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+a = 5
+b = 10
+
+print("{:n} + {:n} = {:n}".format(a, b, add(a, b)))
+print("{:n} - {:n} = {:n}".format(a, b, sub(a, b)))
+print("{:n} / {:n} = {:n}".format(a, b, div(a, b)))
+print("{:n} * {:n} = {:n}".format(a, b, mul(a, b)))
+
+if __name__ == "__main__":
+    add(a, b)
+    sub(a, b)
+    div(a, b)
+    mul(a, b)
